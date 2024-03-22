e9e97c09af747ec0cb8d9f5f9ce1bc0b154367b8
Gustavo Pirondi <gustavopirondi@gmail.com>
2024-03-21 22:16:49 -0300

    Aula 2
    Exercícios da Aula 2
    
    Att* Remoção dos arquivos da Aula Sincronização

---
+++ Aula2/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading; 
+
+class Program
+{
+    static int[] arrey = new int[5];
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < arrey.Length; i++) 
+        {
+            Console.WriteLine("Digite o número na casa " + i + ": ");
+            arrey[i] = int.Parse(Console.ReadLine());
+
+        }
+            Thread t1 = new Thread(() => Soma());
+            Thread t2 = new Thread(() => Soma());
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            int soma = Soma();
+            Console.WriteLine("Soma: " + soma);  
+    }
+
+    static int Soma()
+    {
+        int soma = 0;
+        lock (typeof(Program))
+        {
+            for(int i = 0; i < arrey.Length; i++)
+            {
+                soma += arrey[i];
+            }
+            return soma;
+        }
+    }
+}
---
+++ Aula2/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static string[] arquivo = new string[5];
+    static int i = 0;
+    static void Main(string[] args)
+    {
+
+        for(i = 0; i < arquivo.Length; i++)
+        {
+            Console.WriteLine("Digite um Arquivo (Digite Fim para Finalizar): ");
+            arquivo[i] = Console.ReadLine();
+        }
+
+        Thread t1 = new Thread(() => { Impressora(arquivo[0]); });
+        Thread t2 = new Thread(() => { Impressora(arquivo[1]); });
+        Thread t3 = new Thread(() => { Impressora(arquivo[2]); });
+        Thread t4 = new Thread(() => { Impressora(arquivo[3]); });
+        Thread t5 = new Thread(() => { Impressora(arquivo[4]); });
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+        t4.Start();
+        t5.Start();
+
+        t1.Join();
+        t2.Join();
+        t3.Join();
+        t4.Join();
+        t5.Join();
+
+    }
+
+    static void Impressora(string arquivo)
+    {
+        lock (typeof(Program))
+        {
+            for (i = 0;i < arquivo.Length;i++)
+            {
+                //* Thread.Sleep(5000); *//
+                Console.WriteLine($"Arquivo {i} Impresso -> {arquivo[i]}");;
+            }
+        }
+    }
+}
